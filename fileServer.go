fbaa21d76cd2157aff10f1235782ac24ddb8b260
Victor Mota <vimota@gmail.com>
2014-12-09 11:38:16 +0100

    Implemented allocate file and have all the file structures and json backups constructed

--- fileServer.go
+++ fileServer.go
@@ -5,12 +5,33 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"math/rand"
 	"net"
 	"os"
+	"path"
+	"runtime"
+	"strconv"
+	"time"
 )
 
 const msgBufferLength uint32 = 65546
+
+// random number seeding for fileId creation
+var r = rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
+
+type File struct {
+	FileId             uint32
+	FileSize           uint64
+	RelativePathLength uint16
+	RelativePath       string
+	AbsolutePath       string
+}
+
+var files = make(chan map[string]File, 1)
+var filesLoaded = make(chan bool, 1)
 
 type Request struct {
 	cmd     uint8
@@ -27,6 +48,8 @@ func main() {
 	service := ":1200"
 	listener, err := net.Listen("tcp", service)
 	checkError(err)
+	filesLoaded <- false
+	files <- make(map[string]File)
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -59,23 +82,74 @@ func handleClient(conn net.Conn) {
 
 		switch request.cmd {
 		case 1:
+			// LOGIN
 			fmt.Fprintf(os.Stderr, "Login!\n")
 			var loginSuccess = login(request)
 			if loginSuccess {
 				user.LOGGED_IN = true
 				fmt.Fprintf(os.Stderr, "Logged in!\n")
+				loadFiles()
 				bufRes = respondLogin(request, loginSuccess)
 			} else {
 				bufRes = respondLogin(request, loginSuccess)
 			}
 			sendBuffer(conn, bufRes)
 		case 2:
+			// EXIT
 			fmt.Fprintf(os.Stderr, "Exit!\n")
-			bufRes := wrapCommonHeaders(request, []byte{0x12}, []byte{})
+			storeFiles()
+			bufRes = wrapCommonHeaders(request, []byte{0x12}, []byte{})
 			sendBuffer(conn, bufRes)
 			return
+		case 6:
+			// ALLOCATE_FILE
+			fileId, success := allocateFile(request)
+			fmt.Fprintf(os.Stderr, "fileId %v success %v\n", fileId, success)
+			bufRes = respondAllocateFile(request, fileId, success)
+			sendBuffer(conn, bufRes)
 		}
 	}
+}
+
+func storeFiles() {
+	f := <-files
+	b, err := json.Marshal(f)
+	check(err)
+
+	fmt.Fprintf(os.Stderr, "filesStruct: %+v\n", f)
+	fmt.Fprintf(os.Stderr, "filesStruct marshalled: %v\n", b)
+	err = ioutil.WriteFile("filesDb.txt", b, 0777)
+	check(err)
+	files <- f
+}
+
+func loadFiles() {
+	// loads files json struct from file
+	if <-filesLoaded {
+		// if files have already been loaded
+		// release lock and return
+		filesLoaded <- true
+		return
+	}
+
+	b, err := ioutil.ReadFile("filesDb.txt")
+	if os.IsNotExist(err) {
+		// if file does not exist create file
+		_, err := os.Create("filesDb.txt")
+		if err != nil {
+			panic(err)
+		}
+		b = []byte{}
+	}
+	<-files
+	var f map[string]File
+	json.Unmarshal(b, &f)
+	if f == nil {
+		f = make(map[string]File)
+	}
+	fmt.Fprintf(os.Stderr, "filesStruct: %+v\n", f)
+	files <- f
+	filesLoaded <- true
 }
 
 func sendBuffer(conn net.Conn, bufRes []byte) {
@@ -89,20 +163,30 @@ func sendBuffer(conn net.Conn, bufRes []byte) {
 func login(request *Request) bool {
 	var usernameLength, passwordLength uint8
 	var username, password uint64
-	// convert from bytes to ints
-	binary.Read(bytes.NewReader(request.payload[0:1]), binary.BigEndian, &usernameLength)
-	binary.Read(bytes.NewReader(request.payload[1:5]), binary.BigEndian, &username)
-	binary.Read(bytes.NewReader(request.payload[5:6]), binary.BigEndian, &passwordLength)
-	binary.Read(bytes.NewReader(request.payload[6:10]), binary.BigEndian, &password)
-	fmt.Fprintf(os.Stderr, "username length: %d\n", usernameLength)
-	fmt.Fprintf(os.Stderr, "username: %v\n", username)
-	fmt.Fprintf(os.Stderr, "password length: %d\n", passwordLength)
-	fmt.Fprintf(os.Stderr, "password: %d\n", password)
 
-	if bytes.Equal(request.payload[1:5], request.payload[6:10]) { // username > 1001 && username < 9999 {
+	// convert from bytes to uints
+	binary.Read(bytes.NewReader(request.payload[0:1]), binary.BigEndian, &usernameLength)
+	binary.Read(bytes.NewReader(request.payload[5:6]), binary.BigEndian, &passwordLength)
+
+	// convert ascii bytes to string to uint
+	username, _ = strconv.ParseUint(string(request.payload[1:5]), 10, 64)
+	password, _ = strconv.ParseUint(string(request.payload[6:10]), 10, 64)
+
+	// fmt.Fprintf(os.Stderr, "username length: %d\n", usernameLength)
+	// fmt.Fprintf(os.Stderr, "username: %v\n", password)
+	// fmt.Fprintf(os.Stderr, "password length: %d\n", passwordLength)
+	// fmt.Fprintf(os.Stderr, "password: %v\n", password)
+
+	if username >= 1001 && username <= 9999 && username == password {
 		return true
 	}
 	return false
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
 
 func respondLogin(request *Request, success bool) []byte {
@@ -115,6 +199,65 @@ func respondLogin(request *Request, success bool) []byte {
 	}
 	// add common headers
 	response := wrapCommonHeaders(request, []byte{0x11}, payload)
+
+	//return
+	return response
+}
+
+func allocateFile(request *Request) (uint32, bool) {
+	var relativePathLength uint16
+	var relativePath string
+	var fileSize uint64
+	var fileId uint32
+
+	// convert from bytes to appropriate types
+	binary.Read(bytes.NewReader(request.payload[0:2]), binary.BigEndian, &relativePathLength)
+	relativePath = string(request.payload[2 : 2+relativePathLength])
+	binary.Read(bytes.NewReader(request.payload[2+relativePathLength:]), binary.BigEndian, &fileSize)
+
+	// fmt.Fprintf(os.Stderr, "relative path length: %d\n", relativePathLength)
+	// fmt.Fprintf(os.Stderr, "relativePath: %v\n", relativePath)
+	// fmt.Fprintf(os.Stderr, "fileSize: %v\n", fileSize)
+
+	var empty = make([]byte, fileSize)
+	var err error
+	// create directory in relative path to where program is running
+	_, currentPath, _, _ := runtime.Caller(1)
+	basepath := path.Join(path.Dir(currentPath), path.Dir(relativePath))
+	fmt.Fprintf(os.Stderr, "basepath: %v\n", basepath)
+	err = os.MkdirAll(basepath, 0777)
+	check(err)
+
+	// write file
+	absolutePath := path.Join(basepath, path.Base(relativePath))
+	err = ioutil.WriteFile(absolutePath, empty, 0777)
+	check(err)
+
+	// create file id
+	fileId = r.Uint32()
+	file := File{fileId, fileSize, relativePathLength, relativePath, absolutePath}
+	fmt.Println(file)
+
+	filesTmp := <-files
+	filesTmp[strconv.Itoa(int(fileId))] = file
+	fmt.Println(filesTmp)
+	files <- filesTmp
+	return fileId, true
+}
+
+func respondAllocateFile(request *Request, fileId uint32, success bool) []byte {
+	// make payload
+	var payload []byte
+	if success {
+		payload = []byte{0x01}
+	} else {
+		payload = []byte{0x00}
+	}
+	fileIdBytes := make([]byte, 4)
+	binary.BigEndian.PutUint32(fileIdBytes, fileId)
+	payload = append(payload, fileIdBytes...)
+	// add common headers
+	response := wrapCommonHeaders(request, []byte{0x16}, payload)
 
 	//return
 	return response
