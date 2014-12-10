8a2eed8646fa33ff3a5659bb9be21e21af8f2389
Victor Mota <vimota@gmail.com>
2014-12-10 12:12:56 +0100

    Implemented four more cmds, now handles chunks writing and reading chunk info. Only reading chunks left

--- fileServer.go
+++ fileServer.go
@@ -14,6 +14,7 @@ import (
 	"path"
 	"runtime"
 	"strconv"
+	"syscall"
 	"time"
 )
 
@@ -28,6 +29,13 @@ type File struct {
 	RelativePathLength uint16
 	RelativePath       string
 	AbsolutePath       string
+	Chunks             []Chunk
+}
+
+type Chunk struct {
+	Start  uint64
+	End    uint64
+	Length uint64
 }
 
 var files = make(chan map[string]File, 1)
@@ -101,16 +109,43 @@ func handleClient(conn net.Conn) {
 			bufRes = wrapCommonHeaders(request, []byte{0x12}, []byte{})
 			sendBuffer(conn, bufRes)
 			return
+		case 3:
+			// GET_STORAGE_DATA
+			fmt.Fprintf(os.Stderr, "GetStorageData\n")
+			bufRes = respondGetStorageData(request)
+			sendBuffer(conn, bufRes)
 		case 4:
 			// GET_FILE_IDS
 			fmt.Fprintf(os.Stderr, "GetFileIds\n")
 			bufRes = respondGetFileIds(request)
+			sendBuffer(conn, bufRes)
+		case 5:
+			// GET_FILE_CHUNKS
+			fmt.Fprintf(os.Stderr, "GetFileChunks\n")
+			bufRes = respondGetFileChunks(request)
 			sendBuffer(conn, bufRes)
 		case 6:
 			// ALLOCATE_FILE
 			fileId, success := allocateFile(request)
 			fmt.Fprintf(os.Stderr, "fileId %v success %v\n", fileId, success)
 			bufRes = respondAllocateFile(request, fileId, success)
+			sendBuffer(conn, bufRes)
+		case 7:
+			// WRITE_FILE_CHUNK
+			bufRes = respondWriteFileChunk(request)
+			sendBuffer(conn, bufRes)
+		case 9:
+			// DELETE_FILE
+			fmt.Fprintf(os.Stderr, "DeleteFile\n")
+			fileId, success := deleteFile(request)
+
+			fileIdBytes := make([]byte, 4)
+			binary.BigEndian.PutUint32(fileIdBytes, fileId)
+			if success {
+				bufRes = wrapCommonHeaders(request, []byte{0x19}, []byte{0x01}, fileIdBytes)
+			} else {
+				bufRes = wrapCommonHeaders(request, []byte{0x19}, []byte{0x00}, fileIdBytes)
+			}
 			sendBuffer(conn, bufRes)
 		}
 	}
@@ -240,7 +275,7 @@ func allocateFile(request *Request) (uint32, bool) {
 
 	// create file id
 	fileId = r.Uint32()
-	file := File{fileId, fileSize, relativePathLength, relativePath, absolutePath}
+	file := File{fileId, fileSize, relativePathLength, relativePath, absolutePath, []Chunk{}}
 	fmt.Println(file)
 
 	filesTmp := <-files
@@ -263,6 +298,47 @@ func respondAllocateFile(request *Request, fileId uint32, success bool) []byte {
 	payload = append(payload, fileIdBytes...)
 	// add common headers
 	response := wrapCommonHeaders(request, []byte{0x16}, payload)
+
+	//return
+	return response
+}
+
+func respondGetStorageData(request *Request) []byte {
+	// make payload
+	var payload []byte
+
+	payloadBuf := new(bytes.Buffer)
+
+	// from http://stackoverflow.com/questions/20108520/get-amount-of-free-disk-space-using-go
+	var stat syscall.Statfs_t
+	wd, err := os.Getwd()
+	syscall.Statfs(wd, &stat)
+
+	// Available blocks * size per block = available space in bytes
+	freeSpace := stat.Bavail * uint64(stat.Bsize)
+	totalSpace := stat.Blocks * uint64(stat.Bsize)
+	fmt.Println("freeSpace %v", freeSpace)
+	fmt.Println("totalSpace %v", totalSpace)
+
+	err = binary.Write(payloadBuf, binary.BigEndian, totalSpace)
+	if err != nil {
+		fmt.Println("totalSpace binary.Write failed:", err)
+	}
+
+	err = binary.Write(payloadBuf, binary.BigEndian, freeSpace)
+	if err != nil {
+		fmt.Println("freeSpace binary.Write failed:", err)
+	}
+
+	err = binary.Write(payloadBuf, binary.BigEndian, totalSpace-freeSpace)
+	if err != nil {
+		fmt.Println("usedSpace binary.Write failed:", err)
+	}
+
+	payload = payloadBuf.Bytes()
+
+	// add common headers
+	response := wrapCommonHeaders(request, []byte{0x13}, payload)
 
 	//return
 	return response
@@ -320,12 +396,167 @@ func respondGetFileIds(request *Request) []byte {
 	return response
 }
 
-func wrapCommonHeaders(request *Request, cmd []byte, payload []byte) []byte {
+func respondGetFileChunks(request *Request) []byte {
+	var response []byte
+	var err error
+
+	var fileId uint32
+	binary.Read(bytes.NewReader(request.payload[0:4]), binary.BigEndian, &fileId)
+
+	f := <-files
+	// release files data
+	// in case something wrong here doesn't
+	// block whole program
+	files <- f
+
+	fileInfo, ok := f[strconv.Itoa(int(fileId))]
+	if !ok {
+		// fileId not found in db
+		emptyPayload := make([]byte, 12)
+		response = wrapCommonHeaders(request, []byte{0x15}, emptyPayload)
+		return response
+	}
+
+	chunkBuf := new(bytes.Buffer)
+
+	// CHUNK_DESCRIPTOP per chunk
+	for _, chunk := range fileInfo.Chunks {
+		err = binary.Write(chunkBuf, binary.BigEndian, chunk.Start)
+		if err != nil {
+			fmt.Println("Chunk Start binary.Write failed:", err)
+		}
+
+		err = binary.Write(chunkBuf, binary.BigEndian, chunk.End)
+		if err != nil {
+			fmt.Println("Chunk End binary.Write failed:", err)
+		}
+
+		err = binary.Write(chunkBuf, binary.BigEndian, chunk.Length)
+		if err != nil {
+			fmt.Println("Chunk Length binary.Write failed:", err)
+		}
+	}
+	// convert to bytes
+	chunkBytes := chunkBuf.Bytes()
+
+	payloadBuf := new(bytes.Buffer)
+
+	// success
+	_, err = payloadBuf.Write([]byte{0x01})
+	check(err)
+
+	// fileId
+	err = binary.Write(payloadBuf, binary.BigEndian, fileId)
+	check(err)
+
+	// totalLength
+	err = binary.Write(payloadBuf, binary.BigEndian, uint32(4+len(chunkBytes)))
+	check(err)
+
+	// numberOfChunks
+	err = binary.Write(payloadBuf, binary.BigEndian, uint32(len(fileInfo.Chunks)))
+	check(err)
+
+	// chunk_descriptor per file
+	_, err = payloadBuf.Write(chunkBytes)
+	check(err)
+
+	var payload []byte
+	payload = payloadBuf.Bytes()
+
+	// add common headers
+	response = wrapCommonHeaders(request, []byte{0x15}, payload)
+
+	//return
+	return response
+}
+
+func respondWriteFileChunk(request *Request) []byte {
+	var fileId uint32
+	var start, end, length uint64
+	var data []byte
+	var response []byte
+	binary.Read(bytes.NewReader(request.payload[0:4]), binary.BigEndian, &fileId)
+	binary.Read(bytes.NewReader(request.payload[4:12]), binary.BigEndian, &start)
+	binary.Read(bytes.NewReader(request.payload[12:20]), binary.BigEndian, &end)
+	binary.Read(bytes.NewReader(request.payload[20:28]), binary.BigEndian, &length)
+	data = request.payload[28:]
+
+	// fmt.Println("file id: ", fileId)
+	// fmt.Println("start: ", start)
+	// fmt.Println("end: ", end)
+	// fmt.Println("length: ", length)
+	// fmt.Println("data: ", data)
+
+	f := <-files
+	fileInfo, ok := f[strconv.Itoa(int(fileId))]
+	if !ok {
+		// fileId not found in db
+		files <- f
+		response = wrapCommonHeaders(request, []byte{0x17}, []byte{0x00}, request.payload[0:28])
+		return response
+	}
+
+	// TODO : check that chunk writing to hasn't already been written
+
+	// Open the file for read and write (O_RDRW), append to it if it has
+	// content, create it if it does not exit, use 0666 for permissions
+	// on creation.
+	file, err := os.OpenFile(fileInfo.AbsolutePath, os.O_RDWR, 0777)
+	check(err)
+
+	file.WriteAt(data, int64(start))
+	file.Close()
+
+	// Store chunk in file info
+	newChunk := Chunk{start, end, length}
+	fileInfo.Chunks = append(fileInfo.Chunks, newChunk)
+	f[strconv.Itoa(int(fileId))] = fileInfo
+	fmt.Println("new files struct: ", f)
+
+	files <- f
+	response = wrapCommonHeaders(request, []byte{0x17}, []byte{0x01}, request.payload[0:28])
+	return response
+
+}
+
+func deleteFile(request *Request) (uint32, bool) {
+	var fileId uint32
+	binary.Read(bytes.NewReader(request.payload[0:]), binary.BigEndian, &fileId)
+	f := <-files
+	file, ok := f[strconv.Itoa(int(fileId))]
+	if !ok {
+		// fileId not found in db
+		files <- f
+		return fileId, false
+	}
+
+	err := os.Remove(file.AbsolutePath)
+
+	if err != nil {
+		fmt.Println("Delete file error: ", err)
+		delete(f, strconv.Itoa(int(fileId)))
+		files <- f
+		return fileId, false
+	}
+
+	delete(f, strconv.Itoa(int(fileId)))
+	files <- f
+	return fileId, true
+}
+
+func wrapCommonHeaders(request *Request, cmd []byte, payloads ...[]byte) []byte {
 	// fmt.Fprintf(os.Stderr, "payload: %v\n", payload)
 
 	// find length of payload to attach to header
 	// and convert to bytes
-	var length uint32 = uint32(len(payload)) + 5
+	var payload []byte
+	var payloadLen uint32 = 0
+	for _, payloadItem := range payloads {
+		payloadLen = payloadLen + uint32(len(payloadItem))
+		payload = append(payload, payloadItem...)
+	}
+	var length uint32 = uint32(payloadLen) + 5
 	lengthBytes := make([]byte, 4)
 	binary.BigEndian.PutUint32(lengthBytes, length)
 
